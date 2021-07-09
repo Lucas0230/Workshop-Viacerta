76b8323a6fbdb721e963662c40f09319ec21d95a
Lucas Eduardo Schubert <lucas.eduardo.schubert@gmail.com>
2021-07-08 21:32:19 -0300

    primeiro projeto

---
+++ js/sorteador.js
@@ -0,0 +1,39 @@
+const submeterFormulario = () => {
+
+const rangeInicial = document.getElementById('rangeInicial').value;
+const rangeFinal = document.getElementById('rangeFinal').value;
+const minhaAposta = document.getElementById('minhaAposta').value;
+
+realizarSorteio(+rangeInicial, +rangeFinal, +minhaAposta);
+}
+
+const realizarSorteio = (rangeInicial, rangeFinal, minhaAposta) => {
+
+    if (!rangeInicial) {
+        alert('Informe um range inicial!');
+        return;
+    }
+    if (!rangeFinal) {
+        alert('Informe um range final!');
+        return;
+    }
+    if (!minhaAposta) {
+        alert('Informe a sua aposta!');
+        return;
+    }
+
+    const numeroSorteado = novoSorteio(rangeInicial, rangeFinal);
+
+    if (numeroSorteado === minhaAposta) {
+
+        alert('Você acertou!');
+        return;
+    }
+
+    alert(`Errou, o número sorteado foi ${numeroSorteado}`)
+}
+
+const novoSorteio = (rangeInicial, rangeFinal) => {
+
+    return (Math.floor(Math.random() * (rangeFinal - rangeInicial + 1) + rangeInicial));
+}
